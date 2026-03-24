a8ab66d2e3c8ff08a8a3dfd6603e08b3190f84a7
sodanhama <brainosoham@gmail.com>
2026-03-24 16:23:25 +0530

    modified index.html

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+setInterval(updateTime, 1000);
+
 function updateTime() {var currentTime = new Date().toLocaleString();
 var timeText = document.querySelector('#time');
 timeText.textContent = currentTime;}
-
-setInterval(updateTime, 1000);
